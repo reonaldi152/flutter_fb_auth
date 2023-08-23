376806315f58f032ba0852f0ad0c51d78562adf6
reonaldi152 <reo@isysedge-soft.com>
2023-08-23 14:49:45 +0700

    fixing login fb

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
@@ -7,129 +5,73 @@ void main() {
   runApp(const MyApp());
 }
 
-String prettyPrint(Map json) {
-  JsonEncoder encoder = const JsonEncoder.withIndent('  ');
-  String pretty = encoder.convert(json);
-  return pretty;
-}
-
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  Map<String, dynamic>? _userData;
-  AccessToken? _accessToken;
-  bool _checking = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkIfIsLogged();
-  }
-
-  Future<void> _checkIfIsLogged() async {
-    final accessToken = await FacebookAuth.instance.accessToken;
-    setState(() {
-      _checking = false;
-    });
-    if (accessToken != null) {
-      print("is Logged:::: ${prettyPrint(accessToken.toJson())}");
-      // now you can call to  FacebookAuth.instance.getUserData();
-      final userData = await FacebookAuth.instance.getUserData();
-      // final userData = await FacebookAuth.instance.getUserData(fields: "email,birthday,friends,gender,link");
-      _accessToken = accessToken;
-      setState(() {
-        _userData = userData;
-      });
-    }
-  }
-
-  void _printCredentials() {
-    print(
-      prettyPrint(_accessToken!.toJson()),
-    );
-  }
-
-  Future<void> _login() async {
-    final LoginResult result = await FacebookAuth.instance.login(); // by default we request the email and the public profile
-
-    // loginBehavior is only supported for Android devices, for ios it will be ignored
-    // final result = await FacebookAuth.instance.login(
-    //   permissions: ['email', 'public_profile', 'user_birthday', 'user_friends', 'user_gender', 'user_link'],
-    //   loginBehavior: LoginBehavior
-    //       .DIALOG_ONLY, // (only android) show an authentication dialog instead of redirecting to facebook app
-    // );
-
-    if (result.status == LoginStatus.success) {
-      _accessToken = result.accessToken;
-      _printCredentials();
-      // get the user data
-      // by default we get the userId, email,name and picture
-      final userData = await FacebookAuth.instance.getUserData();
-      // final userData = await FacebookAuth.instance.getUserData(fields: "email,birthday,friends,gender,link");
-      _userData = userData;
-    } else {
-      print(result.status);
-      print(result.message);
-    }
-
-    setState(() {
-      _checking = false;
-    });
-  }
-
-
-  Future<void> _logOut() async {
-    await FacebookAuth.instance.logOut();
-    _accessToken = null;
-    _userData = null;
-    setState(() {});
-  }
+  bool _isLoggedIn = false;
+  Map _userObj = {};
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Facebook Auth Example'),
+          title: Text("Facebook DPL"),
         ),
-        body: _checking
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
+        body: Center(
+          child: Container(
+            width: double.infinity,
+            child: _isLoggedIn == true
+                ? Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        _userData != null ? prettyPrint(_userData!) : "NO LOGGED",
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Nama Profile FB : ${_userObj["name"]}"),
+                      SizedBox(
+                        height: 16,
                       ),
-                      const SizedBox(height: 20),
-                      _accessToken != null
-                          ? Text(
-                              prettyPrint(_accessToken!.toJson()),
-                            )
-                          : Container(),
-                      const SizedBox(height: 20),
-                      CupertinoButton(
-                        color: Colors.blue,
-                        child: Text(
-                          _userData != null ? "LOGOUT" : "LOGIN",
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                        onPressed: _userData != null ? _logOut : _login,
+                      Text("Email FB anda : ${_userObj["email"]}"),
+                      SizedBox(
+                        height: 16,
                       ),
-                      const SizedBox(height: 50),
+                      TextButton(
+                          onPressed: () {
+                            FacebookAuth.instance.logOut().then((value) {
+                              setState(() {
+                                _isLoggedIn = false;
+                                _userObj = {};
+                              });
+                            });
+                          },
+                          child: Text("Logout"))
                     ],
+                  )
+                : Center(
+                    child: ElevatedButton(
+                        onPressed: () {
+                          FacebookAuth.instance.login(permissions: [
+                            "public_profile",
+                            "email"
+                          ]).then((value) {
+                            FacebookAuth.instance
+                                .getUserData()
+                                .then((userData) async {
+                              setState(() {
+                                _isLoggedIn = true;
+                                _userObj = userData;
+                              });
+                            });
+                          });
+                        },
+                        child: Text("Login with Facebook")),
                   ),
-                ),
-              ),
+          ),
+        ),
       ),
     );
   }
